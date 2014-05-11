065884ba95facbc2f23bc6f4953d319d0a6dabda
ragnraok <okone1288@gmail.com>
2014-05-11 18:16:49 +0800

    add article object

--- monoweb/mono/api/objects.py
+++ monoweb/mono/api/objects.py
@@ -14,5 +14,16 @@ def fill_timeline_article_object(title, site, updated):
     result = dict(title=title, site=site, updated=updated.strftime("%Y-%M-%d"))
     return result
 
-def fill_article_object():
-    pass
+def fill_article_object(title, site, updated, content, url):
+    """
+    article object:
+        {
+            title: title,
+            site: site_title,
+            updated: YYYY-MM-DD,
+            content: html content,
+            url: origin article url
+        }
+    """
+    return dict(title=title, site=site, updated=updated.strftime("%Y-%M-%d"),
+            content=content, url=url)
