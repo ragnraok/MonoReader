902c6b5d7b9e0ecb5b6cd1b1ac8cc246ca09876c
ragnraok <okone1288@gmail.com>
2014-05-24 00:26:41 +0800

    change cache policy

--- monoandroid/MonoReader/api/src/main/java/cn/ragnarok/monoreader/api/base/BaseAPIRequest.java
+++ monoandroid/MonoReader/api/src/main/java/cn/ragnarok/monoreader/api/base/BaseAPIRequest.java
@@ -1,6 +1,7 @@
 package cn.ragnarok.monoreader.api.base;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
 import com.android.volley.Response;
@@ -11,6 +12,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 import cn.ragnarok.monoreader.api.util.Constant;
 import cn.ragnarok.monoreader.api.util.ErrorHelper;
@@ -24,31 +26,67 @@ public class BaseAPIRequest extends StringRequest {
 
     public BaseAPIRequest(int method, String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
         super(method, url, listener, errorListener);
+        this.setShouldCache(true);
     }
 
     public BaseAPIRequest(int method, String url, Response.Listener<String> listener, Response.ErrorListener errorListener, String postData) {
         super(method, url, listener, errorListener);
         this.mPostData = postData;
+        this.setShouldCache(true);
     }
 
     @Override
     protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        String json = null;
         try {
-            String json = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+            json = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            json = new String(response.data);
+        }
+        try {
             JSONObject resultObject = new JSONObject(json);
             int errorCode = Integer.parseInt(resultObject.getString("error_code"));
             if (errorCode != Constant.ErrorCode.SUCCESS) {
                 return ErrorHelper.handleError(errorCode); // return Response.error here, so that invoke errorListener
             } else {
                 String data = resultObject.get("data").toString();
-                return Response.success(data, HttpHeaderParser.parseCacheHeaders(response));
+                return Response.success(data, makeCacheEntry(response));
             }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
         return Response.error(new ParseError());
+    }
+
+    private Cache.Entry makeCacheEntry(NetworkResponse response) {
+        if (this.getMethod() == Method.POST) {
+            return HttpHeaderParser.parseCacheHeaders(response); // not cache post request
+        } else {
+            /**
+             * custom response cache, ignore "Cache-Control" header
+             */
+            long now = System.currentTimeMillis();
+            Map<String, String> headers = response.headers;
+            long serverDate = 0;
+            if (headers.get("Date") != null) {
+                serverDate = HttpHeaderParser.parseDateAsEpoch(headers.get("Date"));
+            }
+
+            final long cacheHitButRefreshed = 30 * 60 * 1000; // in 30 minutes cache will be hit, but also refreshed on background
+            final long cacheExpired = 30 * 60 * 1000; // in 30 minutes this cache entry expires completely
+            final long softExpire = now + cacheHitButRefreshed;
+            final long ttl = now + cacheExpired;
+
+            Cache.Entry entry = new Cache.Entry();
+            entry.data = response.data;
+            entry.softTtl = softExpire;
+            entry.ttl = ttl;
+            entry.serverDate = serverDate;
+            entry.responseHeaders = headers;
+
+            return entry;
+        }
     }
 
     @Override
