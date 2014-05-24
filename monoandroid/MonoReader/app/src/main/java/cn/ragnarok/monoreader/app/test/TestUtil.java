d436f46f87c066b98df1ef428c7504e756580353
ragnraok <okone1288@gmail.com>
2014-05-24 10:07:21 +0800

    remove test from git cached

--- monoandroid/MonoReader/app/src/main/java/cn/ragnarok/monoreader/app/test/TestUtil.java
+++
@@ -1,8 +0,0 @@
-package cn.ragnarok.monoreader.app.test;
-
-/**
- * Created by ragnarok on 14-5-22.
- */
-public class TestUtil {
-    public static final String HOST = "http://192.168.10.35:5000/";
-}
