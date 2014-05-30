b03970c75a03ab3166bc24203fc68fd3b4e1e14d
ragnraok <okone1288@gmail.com>
2014-05-30 22:59:49 +0800

    change info cache to disk cache

--- monoweb/mono/cache.py
+++ monoweb/mono/cache.py
@@ -1,15 +1,45 @@
 from flask import current_app
 
 import pickle
+import os
+import time
+import fcntl
+
+class FileLock(object):
+    def __init__(self, filename, *args, **kwargs):
+        self.filename = filename
+        self.open_args = args
+        self.open_kwargs = kwargs
+        self.fileobj = None
+
+    def __enter__(self):
+        f = open(self.filename, *self.open_args, **self.open_kwargs)
+        while True:
+            fcntl.flock(f, fcntl.LOCK_EX)
+            fnew = open(self.filename, *self.open_args, **self.open_kwargs)
+            if os.path.sameopenfile(f.fileno(), fnew.fileno()):
+                fnew.close()
+                break
+            else:
+                f.close()
+                f = fnew
+        self.fileobj = f
+        return f
+
+    def __exit__(self, _exc_type, _exc_value, _trackback):
+        self.fileobj.close()
 
 CACHE_FILE = "disk_cache"
 
-class SimpleDiskCache(object):
+class SimpleCache(object):
     """
-    not thread safe
+    a simple dick cache with file lock
     """
     def __init__(self):
-        self.__cache = {}
+        if not os.path.exists(CACHE_FILE):
+            f = open(CACHE_FILE, "w")
+            f.write(pickle.dumps({"testCache": "testCache"}))
+            f.close()
 
     @classmethod
     def create_instance(cls):
@@ -20,13 +50,23 @@ class SimpleDiskCache(object):
             return cls.__instance
 
     def __setitem__(self, key, value):
-        self.__cache[key] = value
-        dumps_result = pickle.dumps(self.__cache)
-        current_app.logger.info('set key: %s, value: %s' % (key, value))
+        #self.__cache[key] = value
+        with FileLock(CACHE_FILE, "r+") as f:
+            cache = ''.join(f.readlines())
+            cache = pickle.loads(cache)
+            cache[key] = value
+            dumps_result = pickle.dumps(cache)
+            f.seek(0)
+            f.write(dumps_result)
+            f.flush()
+            current_app.logger.info('set key: %s, value: %s' % (key, value))
 
     def __getitem__(self, key):
-        current_app.logger.info("get key: %s, value: %s" % (key, self.__cache.get(key)))
-        return self.__cache.get(key)
+        with FileLock(CACHE_FILE, "r") as f:
+            cache = ''.join(f.readlines())
+            cache = pickle.loads(cache)
+            current_app.logger.info("get key: %s, value: %s" % (key, cache.get(key)))
+            return cache.get(key)
 
     def __len__(self):
         return len(self.__cache)
