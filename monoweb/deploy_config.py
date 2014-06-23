63155ae49bc6d23963c215150e7b572208ace73d
ragnraok <okone1288@gmail.com>
2014-06-23 21:22:39 +0800

    little fixed

--- monoweb/deploy_config.py
+++ monoweb/deploy_config.py
@@ -2,8 +2,8 @@ from mono.config import LOG_DIRS
 
 import os
 
-HOST = ["128.199.169.216"]
-HOST_UESRNAME = "root"
+HOST = [""]
+HOST_UESRNAME = ""
 HOST_PASSWORD = ""
 GIT_URL = "https://github.com/ragnraok/MonoReader.git"
 SUPERVISOR_FILENAME = "supervisord.conf"
